bcb27827211cdeef003243036fd7832bbef987c9
Alexander Lyannoy <alyannoy@c1r5p5.khpi.ucode-connect.study>
2021-04-09 16:36:05 +0300

    t00

--- t00/db.sql
+++ t00/db.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
     login VARCHAR(30) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
     full_name TEXT NOT NULL,
-    mail VARCHAR(50) NOT NULL
+    mail VARCHAR(50) NOT NULL,
+    status ENUM('User', 'Admin') DEFAULT 'User' NOT NULL
 
 );
