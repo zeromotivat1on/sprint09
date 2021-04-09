5374ae57535e28c27feeeb28ee664651619fa4df
Alexander Lyannoy <alyannoy@c1r5p5.khpi.ucode-connect.study>
2021-04-09 16:51:54 +0300

    t02

--- t02/index.php
+++ t02/index.php
@@ -1,3 +1,10 @@
+<?php
+
+    session_start();
+
+    include 'connection/DatabaseConnection.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,7 +13,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Password reminder</title>
+    <title>Remind Password</title>
 
     <link rel="stylesheet" href="style.css">
 
@@ -16,32 +23,40 @@
 
 <body>
 
-    <form class="main_form" action="index.php" method="post">
+    <form id='mn_form' class="main_form" action="index.php" method="post">
 
-    <span>Login</span>
+        <span>Login</span>
         <input type="text" name="login" required></input>
 
-        <span>Password</span>
-        <input type="password" name="password" required></input>
-
-        <span class="error hidden">Incorrect login or password</span>
+        <span id="error" class="error"></span>
 
         <div class="buttons">
 
-            <input class="remindpswd_button button" name="remindpswd_button" type="button" value="Remind password">
+            <input class="remindpswd_button button" name="remindpswd_button" type="submit" value="Remind password">
             <input class="clear_button button" name="clear_button" type="reset" value="Clear">
-            <input class="signin_button button" id="signin_button" name="signin_button" type="submit" value="Sign In" onclick="signin()"></input>
 
         </div>
 
     </form>
 
     <?php
+    
 
+        $conn = new DatabaseConnection('127.0.0.1', NULL, 'root', 'password', 'sword');
 
+        $sql = 'SELECT password, status FROM users WHERE login ='. $_POST['login'];
+        $table_password = $conn->connection->query($sql);
+        $fetch = $table_password->fetch(PDO::FETCH_ASSOC);
+        $password = $fetch['password'];
+
+        if(isset($_POST['remindpswd_button'])) {
+
+            mail('lyannoy.alexander@gmail.com', 'Your Password', $password);
+    
+        }
+    
     ?>
 
 </body>
 
 </html>
-
