389a1c9503c4b124c2db508ea58a0b5b84f5945f
Alexander Lyannoy <alyannoy@c1r5p5.khpi.ucode-connect.study>
2021-04-09 16:35:34 +0300

    t01

--- t01/index.php
+++ t01/index.php
@@ -1,3 +1,11 @@
+<?php
+
+    session_start();
+
+    include 'connection/DatabaseConnection.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,7 +24,7 @@
 
 <body>
 
-    <form class="main_form" action="index.php" method="post">
+    <form id='mn_form' class="main_form" action="index.php" method="post">
 
         <span>Login</span>
         <input type="text" name="login" required></input>
@@ -24,44 +32,83 @@
         <span>Password</span>
         <input type="password" name="password" required></input>
 
-        <span class="error hidden">Incorrect login or password</span>
+        <span id="error" class="error"></span>
 
         <div class="buttons">
 
             <input class="clear_button button" name="clear_button" type="reset" value="Clear">
-            <input class="signin_button button" id="signin_button" name="signin_button" type="submit" value="Sign In" onclick="signin()"></input>
+            <input class="signin_button button" id="signin_button" name="signin_button" type="submit" value="Sign In"></input>
 
         </div>
 
     </form>
     
-    <form class="loggedin_form" action="index.php" method="post">
+    <form id='log_form' class="loggedin_form hidden" action="index.php" method="post">
 
-        <span class="status">Status: <span class="admin">Admin</span></span>
+        <span class="status">Status: <span id="status" class="admin"></span></span>
 
         <div class="buttons">
 
-            <input class="signout_button button" name="signout_button" type="reset" value="Sign out">
+            <input class="signout_button button" name="signout_button" type="submit" value="Sign out"></input>
 
         </div>
 
     </form>
 
     <?php
-
+    
         if(isset($_POST['signin_button'])) {
 
-            $login = $_POST['login'];
-            $password = $_POST['password'];
-            echo "$login<br>$password<br>";
+            if(check_user($_POST['login'], $_POST['password'])) {
+
+                echo '<script> signin(); </script>';
+
+                if($_SESSION['status'] == 'Admin') {
+
+                    echo '<script> statusAdmin(); </script>';
+
+                } else if($_SESSION['status'] == 'User') {
+
+                    echo '<script> statusUser(); </script>';
+
+                }
+
+            } else {
+
+                echo '<script> error(); </script>';
+            }
 
         }
 
-    ?>
+        if(isset($_POST['signout_button'])) {
 
-    <script>
-            var login = '<?php echo $login; ?>'
-    </script>
+            echo '<script> signout(); </script>';
+        }
+
+        function check_user($login, $password) {
+
+            $conn = new DatabaseConnection('127.0.0.1', NULL, 'root', 'password', 'sword');
+
+            $password = crypt($password, 'salt');
+            $sql = "SELECT password, status FROM users WHERE login = \"$login\"";
+            $table_password = $conn->connection->query($sql);
+            $fetch = $table_password->fetch(PDO::FETCH_ASSOC);
+            $table_password = $fetch['password'];
+            $_SESSION['status'] = $fetch['status'];
+
+            if($table_password != $password) {
+
+                return false;
+
+            } else {
+
+                return true;
+
+            }
+            
+        }
+    
+    ?>
 
 </body>
 
--- t01/script.js
+++ t01/script.js
@@ -1,9 +1,36 @@
 function signin() {
 
-    let main_form = document.querySelector('.main_form');
-    main_form.className = 'hidden';
+    let main_form = document.getElementById('mn_form');
+    main_form.className = 'main_form hidden';
 
-    let loggedin_form = document.querySelector('."loggedin_form hidden');
+    let loggedin_form = document.getElementById('log_form');
     loggedin_form.className = 'loggedin_form';
 
+}
+
+function signout() {
+
+    let main_form = document.getElementById('mn_form');
+    main_form.className = 'main_form';
+
+    let loggedin_form = document.getElementById('log_form');
+    loggedin_form.className = 'loggedin_form hidden';
+
+}
+
+function error() {
+
+    document.getElementById('error').innerHTML = 'Incorrect login or password';
+}
+
+function statusAdmin() {
+
+    document.getElementById('status').innerHTML = 'Admin';
+    document.getElementById('status').className = 'admin';
+}
+
+function statusUser() {
+
+    document.getElementById('status').innerHTML = 'User';
+    document.getElementById('status').className = 'user';
 }
